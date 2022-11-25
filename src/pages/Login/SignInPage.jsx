92bfd54344eb47c1722a0f2dc659f090ae70bf1b
arian <arian8ultra@gmail.com>
2022-11-25 17:07:23 +0330

    adding anon sign in

--- src/pages/Login/SignInPage.jsx
+++ src/pages/Login/SignInPage.jsx
@@ -8,10 +8,49 @@ import { Center, Spacer } from "@chakra-ui/react";
 import LinkButton from "../../components/LinkButton";
 import { theme } from "../../../theme/Themes";
 import PersonOutlineRoundedIcon from "@mui/icons-material/PersonOutlineRounded";
-
+import ChatSonLogo from "../../../Images/ChatSonLogo.svg";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 export default function SignInPage() {
   const [Username, setUserName] = React.useState();
   const [Password, setPassword] = React.useState();
+  const [UsernameError, setUsernameError] = React.useState(false);
+  const [UsernameErrorHelperText, setUsernameErrorHelperText] = React.useState("");
+  const [PasswordError, setPasswordError] = React.useState(false);
+  const [PasswordErrorHelperText, setPasswordErrorHelperText] = React.useState("");
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    if (Username == null) {
+      setUsernameError(true);
+      setUsernameErrorHelperText("Enter your Username");
+      console.log("username is null");
+    } else {
+      setUsernameError(false);
+      setUsernameErrorHelperText("");
+    }
+    if (Password == null) {
+      setPasswordError(true);
+      setPasswordErrorHelperText("Enter your Password");
+      console.log("password is null");
+    } else {
+      setPasswordError(false);
+    }
+
+    if (Password == "admin" && Username == "admin") {
+      setTimeout(() => {
+        handleNavigate();
+      }, 500);
+    } else {
+    }
+  };
+  const handleAnonSignIn = () => {
+    setTimeout(() => {
+      handleNavigate();
+    }, 500);
+  };
+
+  const handleNavigate = useCallback(() => navigate("/App/Home", { replace: true }), [navigate]);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -20,7 +59,7 @@ export default function SignInPage() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          px:4,
+          px: 4,
         }}
       >
         <Stack spacing={3} width='100%' justifyContent={"center"} justifyItems={"center"}>
@@ -31,16 +70,29 @@ export default function SignInPage() {
                 width: "60px",
                 height: "60px",
               }}
-              src=''
-            >
-              <LockOutlinedIcon />
-            </Avatar>
+              src={ChatSonLogo}
+            ></Avatar>
           </Center>
           <Typography>Sign In</Typography>
 
-          <TextInputNormal label={"Username"} getText={setUserName} />
-          <TextInputNormal label={"Password"} type='password' getText={setPassword} />
-          <Divider/>
+          <TextInputNormal
+            label={"Username"}
+            getText={setUserName}
+            error={UsernameError}
+            helperText={UsernameErrorHelperText}
+            autoComplete='username'
+          />
+          <TextInputNormal
+            label={"Password"}
+            type='password'
+            getText={setPassword}
+            error={PasswordError}
+            helperText={PasswordErrorHelperText}
+            autoComplete='password'
+            fun={handleSignIn}
+
+          />
+          <Divider />
           <Center>
             <LinkButton text={"Sign In"} width={"50%"} />
           </Center>
@@ -49,6 +101,7 @@ export default function SignInPage() {
               text={"Anonymouse Sign In"}
               width={"100%"}
               icon={<PersonOutlineRoundedIcon />}
+              fun={handleAnonSignIn}
             />
           </Center>
         </Stack>
