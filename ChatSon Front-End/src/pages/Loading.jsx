dc169fee8b5ea617eed88a91ba48a61d60a3c053
arian <arian8ultra@gmail.com>
2022-12-22 01:56:44 +0330

    new htaccess and loading page ready for develope

--- ChatSon Front-End/src/pages/Loading.jsx
+++ ChatSon Front-End/src/pages/Loading.jsx
@@ -6,7 +6,7 @@ import backgroundAnimation from "../../theme/backgroundAnimation.svg";
 function LoadingPage() {
   return (
     <ChakraProvider>
-      <Center width={'100vmax'} height={'100vmin'} backgroundColor={'rgb(0,0,0)'}>
+      <Center width={window.innerWidth} height={window.innerHeight} backgroundColor={'rgb(0,0,0)'}>
         <Spinner color={primaryDark}/>
       </Center>
     </ChakraProvider>
