aa767cd4ee5f4e5fe7f8cda9eb135cfd58681f76
arian <arian8ultra@gmail.com>
2022-12-23 00:28:04 +0330

    alpha head to head mode and whole lot of changes

---
+++ ChatSon Front-End/src/pages/HeadToHead/HeadToHeadPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomeCard from '../components/CustomeCard'
+
+function HeadToHeadPage() {
+  return (
+    <CustomeCard>
+        Hi this 
+    </CustomeCard>
+  )
+}
+
+export default HeadToHeadPage
