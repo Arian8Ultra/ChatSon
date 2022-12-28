71296db37d2d1776e6d813e0d4b8b955547e6c0d
arian <arian8ultra@gmail.com>
2022-12-28 13:06:55 +0330

    whole new bottom navbar component

--- ChatSon Front-End/src/components/Navbar.jsx
+++ ChatSon Front-End/src/components/Navbar.jsx
@@ -1,16 +1,11 @@
 import { Center } from "@chakra-ui/react";
-import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
-import ChatRoundedIcon from "@mui/icons-material/ChatRounded";
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
-import LocalFireDepartmentRoundedIcon from "@mui/icons-material/LocalFireDepartmentRounded";
 import MenuIcon from "@mui/icons-material/Menu";
 import {
   AppBar,
   Box,
   Container,
-  Grid,
   IconButton,
   Menu,
   Stack,
@@ -20,7 +15,6 @@ import {
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import {
-  GlassBackground,
   GlassBackgroundLight,
   GlassPrimary,
   GlassPrimaryLight,
@@ -34,16 +28,10 @@ import useNewChatDrawerStore from "../stores/NewChatDrawerStore";
 import useNewChatModalStore from "../stores/NewChatModalStore";
 import usePageStore from "../stores/PageStore";
 import useSideBarStore from "../stores/SideBarStore";
-import BottomNavButton from "./BottomNavButton";
 import { LabelChipFill, LabelChipFillBig } from "./LabelChip";
 import NavbarMenuButton from "./NavbarMenuButton";
 import PeopleRoundedIcon from "@mui/icons-material/PeopleRounded";
-import {
-  AddCircleOutlineRounded,
-  ChatOutlined,
-  HomeOutlined,
-  LocalFireDepartmentOutlined,
-} from "@mui/icons-material";
+import { BottomNav } from "./BottomNav";
 
 export default function Navbar() {
   const [anchorElUser, setAnchorElUser] = React.useState();
@@ -187,114 +175,4 @@ export default function Navbar() {
     </Box>
   );
 }
-function BottomNav(handleOpenUserMenuB, changeChatDrawer, anchorElUserB, handleCloseUserMenuB) {
-  return (
-    <Box sx={{ display: { xs: "flex", sm: "none" } }}>
-      <AppBar
-        position='fixed'
-        color='primary'
-        sx={{
-          width: "90%",
-          left: (window.innerWidth * 1) / 20,
-          borderRadius: borderRadiuos,
-          backgroundColor: GlassBackground,
-          backdropFilter: "blur(6px)",
-          top: "auto",
-          bottom: 20,
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-        }}
-      >
-        <Toolbar disableGutters>
-          <Grid container my={0} mx={0}>
-            <Grid item xs={2.25}>
-              <Center height={"100%"}>
-                <BottomNavButton
-                  name={"Home"}
-                  link={"Home"}
-                  icon={<HomeOutlined sx={{ width: "90%", height: "35px" }} />}
-                  activeIcon={<HomeRoundedIcon sx={{ width: "90%", height: "35px" }} />}
-                />
-              </Center>
-            </Grid>
-            <Grid item xs={2.25}>
-              <Center height={"100%"}>
-                <BottomNavButton
-                  name={"My ChatSon"}
-                  link={"MyChatSon"}
-                  icon={<ChatOutlined sx={{ width: "90%", height: "35px" }} />}
-                  activeIcon={<ChatRoundedIcon sx={{ width: "90%", height: "35px" }} />}
-                />
-              </Center>
-            </Grid>
 
-            <Grid item xs={3}></Grid>
-
-            <Grid item xs={2.25}>
-              <Center height={"100%"}>
-                <BottomNavButton
-                  name={"Trendings"}
-                  link={"Trendings"}
-                  icon={<LocalFireDepartmentOutlined sx={{ width: "90%", height: "35px" }} />}
-                  activeIcon={
-                    <LocalFireDepartmentRoundedIcon sx={{ width: "90%", height: "35px" }} />
-                  }
-                />
-              </Center>
-            </Grid>
-            <Grid item xs={2.25}>
-              <Center height={"100%"}>
-                <IconButton onClick={handleOpenUserMenuB}>
-                  <AccountCircleRoundedIcon sx={{ color: "white", width: "90%", height: "35px" }} />
-                </IconButton>
-              </Center>
-            </Grid>
-          </Grid>
-
-          <Center position={"fixed"} width={"100%"} left={0} height={0} rounded>
-            <IconButton
-              onClick={changeChatDrawer}
-              sx={{
-                padding: 0,
-              }}
-            >
-              <AddCircleOutlineRounded
-                sx={{
-                  color: "white",
-                  width: "100%",
-                  height: "80px",
-                  background: primaryGradient,
-                  borderRadius: "100px",
-                }}
-              />
-            </IconButton>
-          </Center>
-
-          <Menu
-            anchorEl={anchorElUserB}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "center",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "bottom",
-              horizontal: "center",
-            }}
-            open={Boolean(anchorElUserB)}
-            onClose={handleCloseUserMenuB}
-            PaperProps={{
-              sx: {
-                backgroundColor: GlassBackground,
-                borderRadius: borderRadiuosMenu,
-                backdropFilter: "blur(6px)",
-              },
-            }}
-          >
-            <NavbarMenuButton name='Profile Settings' link='Profile Settings' />
-            <NavbarMenuButton name='Sign Out' link='/' />
-          </Menu>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
