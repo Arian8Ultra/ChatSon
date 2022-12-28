79be953a12cdbf75878382306cc97c08260d7650
arian <arian8ultra@gmail.com>
2022-12-28 13:11:21 +0330

    lazy loading update

--- ChatSon Front-End/src/routes/MyRouter.jsx
+++ ChatSon Front-End/src/routes/MyRouter.jsx
@@ -3,25 +3,24 @@ import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import LoadingPage from "../pages/Loading";
 import ProfileSettings from "../pages/Profile/ProfileSettings";
-import App from "../App";
-import HomePage from "../pages/Home/HomePage";
+// import App from "../App";
+// import HomePage from "../pages/Home/HomePage";
 import LoginPage from "../pages/Login/LoginPage";
 import SignInPage from "../pages/Login/SignInPage";
-import HeadToHeadPage from "../pages/HeadToHead/HeadToHeadPage";
-import SignUpPage from "../pages/Login/SignUpPage";
-import MyChatSonPage from "../pages/MyChatSon/MyChatSonPage";
-import ProfilePage from "../pages/Profile/ProfilePage";
-import TrendingsPage from "../pages/Trendings/TrendingsPage";
+// import HeadToHeadPage from "../pages/HeadToHead/HeadToHeadPage";
+// import SignUpPage from "../pages/Login/SignUpPage";
+// import MyChatSonPage from "../pages/MyChatSon/MyChatSonPage";
+// import ProfilePage from "../pages/Profile/ProfilePage";
+// import TrendingsPage from "../pages/Trendings/TrendingsPage";
 
 export default function MyRouter() {
-  // const App = lazy(() => import("../App"));
-  // const HomePage = lazy(() => import("../pages/Home/HomePage"));
-  // const LoginPage = lazy(() => import("../pages/Login/LoginPage"));
-  // const SignInPage = lazy(() => import("../pages/Login/SignInPage"));
-  // const SignUpPage = lazy(() => import("../pages/Login/SignUpPage"));
-  // const MyChatSonPage = lazy(() => import("../pages/MyChatSon/MyChatSonPage"));
-  // const ProfilePage = lazy(() => import("../pages/Profile/ProfilePage"));
-  // const TrendingsPage = lazy(() => import("../pages/Trendings/TrendingsPage"));
+  const App = lazy(() => import("../App"));
+  const HomePage = lazy(() => import("../pages/Home/HomePage"));
+  const SignUpPage = lazy(() => import("../pages/Login/SignUpPage"));
+  const MyChatSonPage = lazy(() => import("../pages/MyChatSon/MyChatSonPage"));
+  const ProfilePage = lazy(() => import("../pages/Profile/ProfilePage"));
+  const TrendingsPage = lazy(() => import("../pages/Trendings/TrendingsPage"));
+  const HeadToHeadPage = lazy(() => import("../pages/HeadToHead/HeadToHeadPage"));
 
   return (
     <Suspense fallback={<LoadingPage />}>
