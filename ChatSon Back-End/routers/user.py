66d13061d3bb99fe4d6874548e0f4834b8d185af
Mohammadreza Amani <More.Amani@yahoo.com>
2022-12-31 06:22:31 +0330

    users DOCSTRING

--- ChatSon Back-End/routers/user.py
+++ ChatSon Back-End/routers/user.py
@@ -8,14 +8,31 @@ from services.user import user
 router = APIRouter()
 
 
-@router.post('/', response_model=UserDisplay)
+@router.post("/", response_model=UserDisplay)
 def create_user(request: UserBase) -> Any:
+    """
+    Create user
+    :param request: **UserBase**
+    :return: **UserDisplay**
+
+    """
     return user.create(request=request)
 
-@router.get('/{username}', response_model=UserDisplay)
+
+@router.get("/{username}", response_model=UserDisplay)
 def get_user(username: str) -> Any:
+    """
+    Get user
+    :param username: **str**
+    :return: **UserDisplay**
+    """
     return user.get(username=username)
 
-@router.get('/', response_model=list[UserDisplay])
+
+@router.get("/", response_model=list[UserDisplay])
 def get_users() -> Any:
+    """
+    Get all users
+    :return: **list[UserDisplay]**
+    """
     return user.get_all()
