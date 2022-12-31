6c7c428b8ad7055596c4dc9a7481f3af9575529d
Mohammadreza Amani <More.Amani@yahoo.com>
2022-12-31 06:07:40 +0330

    ADDING GET USER AND LIST OF ALL USERS

--- ChatSon Back-End/routers/user.py
+++ ChatSon Back-End/routers/user.py
@@ -11,3 +11,11 @@ router = APIRouter()
 @router.post('/', response_model=UserDisplay)
 def create_user(request: UserBase) -> Any:
     return user.create(request=request)
+
+@router.get('/{username}', response_model=UserDisplay)
+def get_user(username: str) -> Any:
+    return user.get(username=username)
+
+@router.get('/', response_model=list[UserDisplay])
+def get_users() -> Any:
+    return user.get_all()
