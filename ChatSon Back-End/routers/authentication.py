508acee843d4a8716e8d3a2cb63e59797de0a737
Mohammadreza Amani <More.Amani@yahoo.com>
2022-12-31 06:12:31 +0330

    PEP8

--- ChatSon Back-End/routers/authentication.py
+++ ChatSon Back-End/routers/authentication.py
@@ -10,30 +10,32 @@ from utils.hash import Hash
 router = APIRouter()
 
 
-@router.post('/')
+@router.post("/")
 def login(request: OAuth2PasswordRequestForm = Depends()):
     with SessionLocal() as session:
         user = session.query(User).filter(User.username == request.username).first()
 
         if not user:
-            raise HTTPException(detail='User not found',
-                                status_code=status.HTTP_404_NOT_FOUND)
+            raise HTTPException(
+                detail="User not found", status_code=status.HTTP_404_NOT_FOUND
+            )
         if not Hash.verify(user.password, request.password):
-            raise HTTPException(detail='Incorrect password',
-                                status_code=status.HTTP_404_NOT_FOUND)
+            raise HTTPException(
+                detail="Incorrect password", status_code=status.HTTP_404_NOT_FOUND
+            )
 
-        access_token = create_access_token(request={'username': user.username})
-        
+        access_token = create_access_token(request={"username": user.username})
+
         if not user.is_active:
             user.is_active = True
             session.commit()
             # session.refresh(user)
 
         return {
-            'access_token': access_token,
-            'token_type': 'bearer',
-            'user_id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'is_active': user.is_active
+            "access_token": access_token,
+            "token_type": "bearer",
+            "user_id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "is_active": user.is_active,
         }
